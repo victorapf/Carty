f8dd116d9b78f32d050282af1a1ab03e40c3e247
lioneljose <98002508+lioneljose@users.noreply.github.com>
2023-07-13 23:21:06 -0400

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/FQAs')
+def FQAs():
+    return render_template('FQAs.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+   
